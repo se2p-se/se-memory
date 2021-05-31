55072670954ac428e3dee98af1af8bf9edf3e175
michaelgrantner <michael.grantner@t-online.de>
2021-05-31 16:13:04 +0200

    Cards implementiert

--- src/main/java/de/uni_passau/fim/se/memory/model/Card.java
+++ src/main/java/de/uni_passau/fim/se/memory/model/Card.java
@@ -1,8 +1,26 @@
 package de.uni_passau.fim.se.memory.model;
 
 public class Card {
-	public Card(Character ch) {
+	private char value;
+	private boolean isHidden = true ;
+	private static String hiddenValue = "[]" ;
 
+	public Card(Character ch) {
+		this.value = ch ;
 	}
 
+	public char getValue () {
+		return value ;
+	}
+	public void setValue (char wert) {
+		value = wert ;
+	}
+	public void flipCard () {
+		if (isHidden) {
+			isHidden = false ;
+		}
+		else {
+			isHidden = true ;
+		}
+	}
 }
