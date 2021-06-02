571ca7b174e710c2d026475b88ce7589c6fdaddc
Valentin Damjantschitsch <damjan03@stud.uni-passau.de>
2021-06-02 15:27:20 +0200

    Moved selectCard, removeCards, compareCards to from class Card.java to class Game.java

--- src/main/java/de/uni_passau/fim/se/memory/model/Card.java
+++ src/main/java/de/uni_passau/fim/se/memory/model/Card.java
@@ -20,33 +20,5 @@ public class Card {
 		isHidden = !isHidden ;
 	}
 
-	/**
-	 * Compares the values of @param card1 and @param card2
-	 * If the values are identical call remove on these cards and @return true, otherwise false.
-	 */
-	public static boolean compareCards(Card card1, Card card2) {
-		if (card1.value == card2.value) {
-			removeCard(card1, card2);
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Selects the card at @param position of @param game
-	 * @return the selected Card
-	 */
-	public static Card selectCard(Game game, int position) {
-		return game.getCards().get(position);
-	}
-
-	/**
-	 * Removes @param card by setting its value to null
-	 */
-	public static void removeCard(Card card1, Card card2) {
-		card1.value = null;
-		card2.value = null;
-	}
 }
 
