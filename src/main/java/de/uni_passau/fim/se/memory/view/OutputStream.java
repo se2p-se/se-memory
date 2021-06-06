71f02803b8767d074ba6de9a69a84788f18a58ab
KLichtenauer <kilian22lichtenauer@gmail.com>
2021-06-06 14:53:44 +0200

    Letzte Fehler behoben

--- src/main/java/de/uni_passau/fim/se/memory/view/OutputStream.java
+++ src/main/java/de/uni_passau/fim/se/memory/view/OutputStream.java
@@ -7,68 +7,69 @@ public class OutputStream {
 
     public static void printBoard(Game board) {
         System.out.println(board.toString());
-
-    public static void printIntro() {
-        System.out.println(
-                " /$$      /$$ /$$$$$$$$ /$$      /$$  /$$$$$$  /$$$$$$$  /$$     /$$\n"
-                        + "| $$$    /$$$| $$_____/| $$$    /$$$ /$$__  $$| $$__  $$|  $$   /$$/\n"
-                        + "| $$$$  /$$$$| $$      | $$$$  /$$$$| $$  \\ $$| $$  \\ $$ \\  $$ /$$/ \n"
-                        + "| $$ $$/$$ $$| $$$$$   | $$ $$/$$ $$| $$  | $$| $$$$$$$/  \\  $$$$/  \n"
-                        + "| $$  $$$| $$| $$__/   | $$  $$$| $$| $$  | $$| $$__  $$   \\  $$/   \n"
-                        + "| $$\\  $ | $$| $$      | $$\\  $ | $$| $$  | $$| $$  \\ $$    | $$    \n"
-                        + "| $$ \\/  | $$| $$$$$$$$| $$ \\/  | $$|  $$$$$$/| $$  | $$    | $$    \n"
-                        + "|__/     |__/|________/|__/     |__/ \\______/ |__/  |__/    |__/    \n"
-                        + "                                                                    \n");
     }
 
-    public static void printEndOfGame() {
-        System.out.println("Your game of Memory is over. You uncovered all pairs." +
-                " Hope you have had a lot of fun and come back soon");
-    }
+        public static void printIntro () {
+            System.out.println(
+                    " /$$      /$$ /$$$$$$$$ /$$      /$$  /$$$$$$  /$$$$$$$  /$$     /$$\n"
+                            + "| $$$    /$$$| $$_____/| $$$    /$$$ /$$__  $$| $$__  $$|  $$   /$$/\n"
+                            + "| $$$$  /$$$$| $$      | $$$$  /$$$$| $$  \\ $$| $$  \\ $$ \\  $$ /$$/ \n"
+                            + "| $$ $$/$$ $$| $$$$$   | $$ $$/$$ $$| $$  | $$| $$$$$$$/  \\  $$$$/  \n"
+                            + "| $$  $$$| $$| $$__/   | $$  $$$| $$| $$  | $$| $$__  $$   \\  $$/   \n"
+                            + "| $$\\  $ | $$| $$      | $$\\  $ | $$| $$  | $$| $$  \\ $$    | $$    \n"
+                            + "| $$ \\/  | $$| $$$$$$$$| $$ \\/  | $$|  $$$$$$/| $$  | $$    | $$    \n"
+                            + "|__/     |__/|________/|__/     |__/ \\______/ |__/  |__/    |__/    \n"
+                            + "                                                                    \n");
+        }
 
-    public static void printInvalidInput() {
-        System.out.println("Your input was invalid. Please try again: ");
-    }
+        public static void printEndOfGame () {
+            System.out.println("Your game of Memory is over. You uncovered all pairs." +
+                    " Hope you have had a lot of fun and come back soon");
+        }
 
-    public static void printSelectCol1() {
-        System.out.println("Choose card 1 and write the corresponding col (Only chose Ints between 1 - 5) : ");
-    }
+        public static void printInvalidInput () {
+            System.out.println("Your input was invalid. Please try again: ");
+        }
 
-    public static void printSelectRow1() {
-        System.out.println("Choose card 1 and write the corresponding row (Only chose Ints between 1 - 4) : ");
-    }
+        public static void printSelectCol1 () {
+            System.out.println("Choose card 1 and write the corresponding col (Only chose Ints between 1 - 5) : ");
+        }
 
-    public static void printSelectCol2() {
-        System.out.println("Choose card 2 and write the corresponding col (Only chose Ints between 1 - 5) : ");
-    }
+        public static void printSelectRow1 () {
+            System.out.println("Choose card 1 and write the corresponding row (Only chose Ints between 1 - 4) : ");
+        }
 
-    public static void printSelectRow2() {
-        System.out.println("Choose card 2 and write the corresponding row (Only chose Ints between 1 - 4) : ");
-    }
+        public static void printSelectCol2 () {
+            System.out.println("Choose card 2 and write the corresponding col (Only chose Ints between 1 - 5) : ");
+        }
 
-    public static void sameCardsChosen() {
-        System.out.println("You chose the same cards, pleas choose the second card again: ");
-    }
+        public static void printSelectRow2 () {
+            System.out.println("Choose card 2 and write the corresponding row (Only chose Ints between 1 - 4) : ");
+        }
 
-    public static void chosenCard2IsNull() {
-        System.out.println("The second card you chose is invalid, please "
-                + "select again:");
-    }
+        public static void sameCardsChosen () {
+            System.out.println("You chose the same cards, pleas choose the second card again: ");
+        }
 
-    public static void chosenCard1IsNull() {
-        System.out.println("The first card you chose is invalid, please "
-                + "select again: ");
-    }
+        public static void chosenCard2IsNull () {
+            System.out.println("The second card you chose is invalid, please "
+                    + "select again:");
+        }
 
-    public static void pairFound(Card card) {
-        System.out.println("Congrats you found a pair with the value " + card.getValue() + "!");
-    }
+        public static void chosenCard1IsNull () {
+            System.out.println("The first card you chose is invalid, please "
+                    + "select again: ");
+        }
 
-    public static void noPairFound() {
-        System.out.println("No pair found, try again.");
-    }
+        public static void pairFound (Card card){
+            System.out.println("Congrats you found a pair with the value " + card.getValue() + "!");
+        }
 
-    public static void nextRound(){
-        System.out.println("Starting next round.");
+        public static void noPairFound () {
+            System.out.println("No pair found, try again.");
+        }
+
+        public static void nextRound () {
+            System.out.println("Starting next round.");
+        }
     }
-}
