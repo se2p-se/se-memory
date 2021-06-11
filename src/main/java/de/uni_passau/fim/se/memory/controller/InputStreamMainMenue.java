4068a436f6bbdf9f9b9d1be6b3be76863f502026
michaelgrantner <michael.grantner@t-online.de>
2021-06-11 13:42:31 +0200

    added Quit-Button to MainMenue

--- src/main/java/de/uni_passau/fim/se/memory/controller/InputStreamMainMenue.java
+++ src/main/java/de/uni_passau/fim/se/memory/controller/InputStreamMainMenue.java
@@ -19,45 +19,49 @@ public class InputStreamMainMenue {
      */
     public void pickMainMenueOptioin () {
         int option = menueScanner.nextInt() ;
-        switch (option) {
-            case 1 :
-                player.gameLoop();
-                break ;
-            case 2 :
-                OutputStreamMainMenue.showModeTime();
-                OutputStreamMainMenue.showModeBot();
-                int modeOption = modeScanner.nextInt();
-                switch (modeOption) {
-                    case 1:
-                        mainMenue.setGameModeTime(true);
-                        mainMenue.setGameModeBot(false);
-                        break ;
-                    case 2 :
-                        mainMenue.setGameModeBot(true);
-                        mainMenue.setGameModeTime(false);
-                        break ;
-                }
-                break;
-            case 3 :
-                OutputStreamMainMenue.showPleaseInsertGameBoardSizeX();
-                int x = setGameBoardSize.nextInt();
-                OutputStreamMainMenue.showPleaseInsertGameBoardSizeY();
-                int y = setGameBoardSize.nextInt();
-                game.setGameBoardSize(x, y);
-                break ;
-            case 4 :
-                if (mainMenue.getActivateHelp() == true) {
-                    mainMenue.setActivateHelp(false);
-                    OutputStreamMainMenue.showActivateHelpSetFalse();
-                }
-                else {
-                    mainMenue.setActivateHelp(true);
-                    OutputStreamMainMenue.showActivateHelpSetTrue();
-                }
-                mainMenue.setTitleActivateHelp();
-                break ;
+        if (option == 5) {
+
         }
-        OutputStreamMainMenue.showMainMenue();
-        pickMainMenueOptioin();
+        else {
+            switch (option) {
+                case 1:
+                    player.gameLoop();
+                    break;
+                case 2:
+                    OutputStreamMainMenue.showModeTime();
+                    OutputStreamMainMenue.showModeBot();
+                    int modeOption = modeScanner.nextInt();
+                    switch (modeOption) {
+                        case 1:
+                            mainMenue.setGameModeTime(true);
+                            mainMenue.setGameModeBot(false);
+                            break;
+                        case 2:
+                            mainMenue.setGameModeBot(true);
+                            mainMenue.setGameModeTime(false);
+                            break;
+                    }
+                    break;
+                case 3:
+                    OutputStreamMainMenue.showPleaseInsertGameBoardSizeX();
+                    int x = setGameBoardSize.nextInt();
+                    OutputStreamMainMenue.showPleaseInsertGameBoardSizeY();
+                    int y = setGameBoardSize.nextInt();
+                    game.setGameBoardSize(x, y);
+                    break;
+                case 4:
+                    if (mainMenue.getActivateHelp() == true) {
+                        mainMenue.setActivateHelp(false);
+                        OutputStreamMainMenue.showActivateHelpSetFalse();
+                    } else {
+                        mainMenue.setActivateHelp(true);
+                        OutputStreamMainMenue.showActivateHelpSetTrue();
+                    }
+                    mainMenue.setTitleActivateHelp();
+                    break;
+            }
+            OutputStreamMainMenue.showMainMenue();
+            pickMainMenueOptioin();
+        }
     }
 }
