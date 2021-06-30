25a29028788dc662129ec0e33c3b54235ae8c582
michaelgrantner <michael.grantner@t-online.de>
2021-06-30 09:32:15 +0200

    Scene Wechsel zu Methoden hinzugefügt

--- src/main/java/de/uni_passau/fim/se/memory/controller/Controller.java
+++ src/main/java/de/uni_passau/fim/se/memory/controller/Controller.java
@@ -17,30 +17,39 @@ public class Controller {
 
 
     @FXML public void back(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        GUI.switchScene(stage, "MainMenueTest.fxml");
     }
 
     @FXML public void playAgainstTime(ActionEvent event) throws IOException{
         mainMenue.setGameModeTime(true);
         mainMenue.setGameModeBot(false);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        GUI.switchScene(stage, "MainMenueTest.fxml");
     }
 
     @FXML public void playAgainstBot(ActionEvent event) throws IOException{
         mainMenue.setGameModeBot(true);
         mainMenue.setGameModeTime(false);
-
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         GUI.switchScene(stage, "MainMenueTest.fxml");
     }
 
     @FXML public void easyBoard(ActionEvent event) throws IOException{
         game.setGameBoardSize(3,4);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        GUI.switchScene(stage, "MainMenueTest.fxml");
     }
 
     @FXML public void mediumBoard(ActionEvent event) throws IOException{
         game.setGameBoardSize(4,4);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        GUI.switchScene(stage, "MainMenueTest.fxml");
     }
 
     @FXML public void difficultBoard(ActionEvent event) throws IOException {
         game.setGameBoardSize(5,4);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        GUI.switchScene(stage, "MainMenueTest.fxml");
     }
 }
