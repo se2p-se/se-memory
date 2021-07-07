628600a4cce6f19ea9fab4ce501195b109ac0732
michaelgrantner <michael.grantner@t-online.de>
2021-07-07 14:00:32 +0200

    prepared tests

--- src/test/java/de/uni_passau/fim/se/testing/Test.java
+++ src/test/java/de/uni_passau/fim/se/testing/Test.java
@@ -17,4 +17,47 @@ class TestView {
                 assertEquals (1, 1);
         }
 
+
+        /**
+         * #297
+         */
+        @Test
+        void testMain () {
+                // is not working yet
+        }
+
+        /**
+         * #298
+         */
+        @Test
+        void testOutputStream () {
+
+        }
+
+
+        /**
+         * #299
+         */
+        @Test
+        void testOutputStreamGameModeBot () {
+
+        }
+
+
+        /**
+         * #300
+         */
+        @Test
+        void testOutputStreamGameModeTime () {
+
+        }
+
+        /**
+         * #301
+         */
+        @Test
+        void testOutputStreamMainMenu () {
+
+        }
+
 }
