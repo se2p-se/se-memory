035ea16d4fec2f729fdde06673d495c64c388cad
michaelgrantner <michael.grantner@t-online.de>
2021-07-07 09:44:42 +0200

    renamed TestView

--- src/test/java/de/uni_passau/fim/se/testing/Test.java
+++ src/test/java/de/uni_passau/fim/se/testing/Test.java
@@ -3,12 +3,12 @@ package de.uni_passau.fim.se.testing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Test1 {
-        private Test1 test;
+class TestView {
+        private TestView test;
 
         @BeforeEach
         void setUp () {
-            test = new Test1();
+            test = new TestView();
         }
 
         @Test
