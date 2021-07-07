991ec09f3c00b37fb81159a5235a611b2f6fefd9
michaelgrantner <michael.grantner@t-online.de>
2021-07-07 10:15:02 +0200

    import assertEquals

--- src/test/java/de/uni_passau/fim/se/testing/Test.java
+++ src/test/java/de/uni_passau/fim/se/testing/Test.java
@@ -3,6 +3,8 @@ package de.uni_passau.fim.se.testing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TestView {
         private TestView test;
 
@@ -13,6 +15,7 @@ class TestView {
 
         @Test
         void stringExtend () {
+                assertEquals(1, 1);
         }
 
 }
